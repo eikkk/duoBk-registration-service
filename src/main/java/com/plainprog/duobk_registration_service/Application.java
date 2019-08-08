ea983dcac974807f454db2c5bb4563fac8bb0cca
EIK <petrodarchyn@gmail.com>
2019-08-08 21:39:16 +0300

    Followed tutorial

--- src/main/java/com/plainprog/duobk_registration_service/Application.java
+++ src/main/java/com/plainprog/duobk_registration_service/Application.java
@@ -2,11 +2,14 @@ package com.plainprog.duobk_registration_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class Application {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "registration-server");
 		SpringApplication.run(Application.class, args);
 	}
 
