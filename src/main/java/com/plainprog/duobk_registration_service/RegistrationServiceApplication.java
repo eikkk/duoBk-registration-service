5568d029521a6e96c224a875b0ce7f8f56277a8c
EIK <petrodarchyn@gmail.com>
2020-03-28 12:35:35 +0200

    renamed application

--- src/main/java/com/plainprog/duobk_registration_service/RegistrationServiceApplication.java
+++ src/main/java/com/plainprog/duobk_registration_service/RegistrationServiceApplication.java
@@ -6,11 +6,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class Application {
+public class RegistrationServiceApplication {
 
 	public static void main(String[] args) {
 		System.setProperty("spring.config.name", "registration-server");
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(RegistrationServiceApplication.class, args);
 	}
 
 }
